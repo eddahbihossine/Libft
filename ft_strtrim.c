b59e22b529ce7700cca51e62c52ebaba81f2959f
Hossine Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2022-10-27 00:30:40 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,37 +1,47 @@
 #include "libft.h"
 
-static int		is_in_set(char c, const char *set)
-{
-	while (*set)
-		if (c == *set++)
-			return (0);
-	return (1);
-}
+#include "libft.h"
 
-char			*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	start;
-	size_t	end;
-	char	*rtn;
-    
+/*This function allocates memory and returns a copy of the string passed in the
+ * parameter but without any kind of blank spaces at the start or the end of
+ * the string. This function considers blank spaces to be the characters ' ',
+ * a standard space, '\n', a new line, and '\t', which is a tabulation space.
+ * If there are no spaces at the beginning and end of the parameter string s
+ * the function returns a copy of s. If the allocation of memory fails the
+ * function returns NULL.*/
 
-	if (!s1)
+char		*ft_strtrim(char const *s)
+{
+	/*We start the function by delcaring four variables. The first three will
+	 * be used as index counting variables. Once again we use unsigned in case
+	 * we are dealing with a ridiculously long string. We also declare a new
+	 * string str which is going to hold and return a copy of our parameter
+	 * string.*/
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	k;
+	char			*str;
+
+	i = 0;
+	k = 0;
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	if (s[i] == '\0')
+		return (ft_strcpy(ft_memalloc(sizeof(char) * 2), ""));
+
+	j = ft_strlen(s) - 1;
+	while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
+		j--;
+
+	str = (char *)malloc(sizeof(char) * (j - i + 2));
+	if (str == NULL)
 		return (NULL);
-	if (!set)
-		return (ft_strdup(s1));
-	start = 0;
-	end = ft_strlen(s1);
-	while (is_in_set(s1[start], set) == 0)
-		start++;
-	if (start == ft_strlen(s1))
+	
+	while (k < j - i + 1)
 	{
-		if (!(rtn = ft_strdup("")))
-			return (NULL);
-		else
-			return (rtn);
+		str[k] = s[i + k];
+		k++;
 	}
-	while (is_in_set(s1[end - 1], set) == 0)
-		end--;
-	rtn = ft_substr(s1, start, end - start);
-	return (rtn);
+	str[k] = '\0';
+	return (str);
 }
