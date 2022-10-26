0a52f8a8acb38159f06211954090f7082ca1e08b
Hossine Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2022-10-27 00:24:28 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,26 +1,24 @@
 
 #include "libft.h"
 
-char *ft_strnstr(const char * s1, const char * s2, size_t len)
+#include "libft.h"
+
+char		*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-    size_t i;
-    size_t j;
-    i = 0;
-    j = 0;
-  
-    if(s2[j] == '\0')
-    return((char *)s1);
-    while(s1[i] && i < len)
-    {
-        while(s1[i + j] == s2[j] && s1[i + j] != '\0')
-       
-            j++;
-            if(s2[j] == '\0')
-            return((char *)s1 + i);
-            j = 0;
-            i++;
-    }
-    return(0);
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < n && s1[i] != '\0')
+	{
+		j = 0;
+		while (i + j < n && s1[i + j] == s2[j] && s2[j] != '\0')
+			j++;
+		if (s2[j] == '\0')
+			return ((char *)s1 + i);
+		i++;
+	}
+	return (NULL);
 }
 int main()
 {
