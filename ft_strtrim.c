2eb2a8aba097f3d2334fb91d7a5fdd53bfbf4554
Hossine Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2022-10-27 00:31:14 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,22 +1,7 @@
 #include "libft.h"
-
-#include "libft.h"
-
-/*This function allocates memory and returns a copy of the string passed in the
- * parameter but without any kind of blank spaces at the start or the end of
- * the string. This function considers blank spaces to be the characters ' ',
- * a standard space, '\n', a new line, and '\t', which is a tabulation space.
- * If there are no spaces at the beginning and end of the parameter string s
- * the function returns a copy of s. If the allocation of memory fails the
- * function returns NULL.*/
-
 char		*ft_strtrim(char const *s)
 {
-	/*We start the function by delcaring four variables. The first three will
-	 * be used as index counting variables. Once again we use unsigned in case
-	 * we are dealing with a ridiculously long string. We also declare a new
-	 * string str which is going to hold and return a copy of our parameter
-	 * string.*/
+
 	unsigned int	i;
 	unsigned int	j;
 	unsigned int	k;
