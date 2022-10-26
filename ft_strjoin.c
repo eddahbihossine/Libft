4de821e4aaf577aec80dd89248eaa6c9425ef4ce
Hossine Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2022-10-27 00:21:19 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,41 +1,27 @@
-
- static int	count_len(int size, char **strs, char *sep)
-{
-	int	len;
-	int	i;
-
-	i = 0;
-	len = 0;
-	while (i < size)
-		len += ft_strlen(strs[i++]);
-	i++;
-	len += (size - 1) * ft_strlen(sep) + 1;
-	return (len);
-}
-
-char	*ft_strjoin(int size, char **strs, char *sep)
+#include "libft.h"
+char		*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
-	int		a;
-	int		b;
-	int		len;
-	char	*p;
-
+	int		j;
+	char	*str;
+	
 	i = 0;
-	a = 0;
-	b = 0;
-	len = count_len(size, strs, sep);
-	p = malloc(sizeof(char) * len);
-	while (a < size)
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	
+	while (s1[i] != '\0')
 	{
-		b = 0;
-		while (strs[a][b])
-			p[i++] = strs[a][b++];
-		b = 0;
-		while (sep[b] && a < size - 1)
-			p[i++] = sep[b++];
-		a++;
+		str[i] = s1[i];
+		i++;
 	}
-	p[i] = '\0';
-	return (p);
+
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
 }
