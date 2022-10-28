2dd613191e68dae8911431965e6f465cb556dca3
Hossine Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2022-10-28 04:29:23 +0100

    Update ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -1,7 +1,15 @@
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void ft_lstadd_front(t_list **lst, t_list *zb)
 {
-	new->next = *lst;
-	*lst = new;
+    (*zb).next = *lst;
+    lst = &zb;
+}
+
+int main()
+{
+  t_list **node;
+  t_list *new_node;
+  
+  ft_lstadd_front(node,new_node);
 }
