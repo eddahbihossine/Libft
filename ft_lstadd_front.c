b80a5755ad72d2e8021150d93c9844a11ac41bed
Hossine Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2022-10-28 05:32:29 +0100

    Update ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -8,8 +8,10 @@ void ft_lstadd_front(t_list **lst, t_list *zb)
 
 int main()
 {
-  t_list **node;
-  t_list *new_node;
+  t_list **qalwa = malloc(sizeof(t_list));
+   t_list *root =&qalwa;
+  t_list *zb= malloc(sizeof(t_list));
+  ft_lstadd_front(qalwa,zb);
   
-  ft_lstadd_front(node,new_node);
+  
 }
